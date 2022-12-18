93c0771435df74eb63eb35ff2bf628fcb2652aae
AleksandrRogov25 <104849405+AleksandrRogov25@users.noreply.github.com>
2022-12-18 17:18:27 +0300

    Сформировал data class с двумя свойствами и вывел их на экран приложения, вызвал copy и так же вывел на экран

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlin
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,14 +8,19 @@ import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView: TextView = findViewById(R.id.textView)
+        val noteOne = Note("Понедельник", "Заметка")
+        val noteTwo = noteOne.copy()
+        val textViewOne: TextView = findViewById(R.id.textViewOne)
+        val textViewTwo: TextView = findViewById(R.id.textViewTwo)
 
         val button: Button = findViewById(R.id.button_test)
         button.setOnClickListener {
-            textView.text = "Кнопка нажата!"
+            textViewOne.text = noteOne.title + " " + noteOne.description
+            textViewTwo.text = noteTwo.title + " " +  noteTwo.description
         }
     }
 }
---
+++ app/src/main/java/com/example/kotlin/Note.kt
@@ -0,0 +1,3 @@
+package com.example.kotlin
+
+data class Note(val title: String, val description: String )
