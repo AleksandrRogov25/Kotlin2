01e312c5989f73d8a7122532f8c4e55f083902d6
AleksandrRogov25 <104849405+AleksandrRogov25@users.noreply.github.com>
2022-12-18 16:30:59 +0300

    Добавил кнопку в разметку и повесил на неё clickListener в Activity

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.textView)
+
+        val button: Button = findViewById(R.id.button_test)
+        button.setOnClickListener {
+            textView.text = "Кнопка нажата!"
+        }
     }
 }
